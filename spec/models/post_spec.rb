ad12556cdb6458f52c17f28dda513982cf6738b3
Filip Persson <filip.tillberg.persson@gmail.com>
2022-03-18 15:23:38 +0100

    implemented creation process for posts and refactoring

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Post, type: :model do
     before do
       @post = Post.create(date: Date.today, rationale: "Anything")
     end
-    it "can be crated" do
+    it "can be created" do
       expect(@post).to be_valid
     end
     it "cannot be created without a date and a rationale" do
