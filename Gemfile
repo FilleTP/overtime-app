e921c94d1092b45f96e3d74031f199da22fa3e1d
Filip Persson <filip.tillberg.persson@gmail.com>
2022-03-31 14:37:54 +0200

    implemented layout for homepage

--- Gemfile
+++ Gemfile
@@ -61,3 +61,5 @@ gem 'devise', '~> 4.8'
 gem "autoprefixer-rails", "10.2.5"
 gem "font-awesome-sass", "~> 5.6.1"
 gem "simple_form", github: 'heartcombo/simple_form'
+gem 'derailed_benchmarks', group: :development
+gem 'stackprof', group: :development
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,9 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+import "bootstrap"
+import 'bootstrap/js/src/collapse'
+import 'bootstrap/js/src/dropdown'
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
