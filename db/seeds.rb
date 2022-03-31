3c52b75094b06ed43afb799f196ec82bab4b87a8
Filip Persson <filip.tillberg.persson@gmail.com>
2022-03-31 15:51:54 +0200

    updated seedfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
+@user = User.create(email: "newmeail@cool.com", password: "12345671", password_confirmation: "12345671", first_name: "Peter", last_name: "Andersson")
+puts "1 user created"
 100.times do |post|
-  Post.create(date: Date.today, rationale: "#{post} rationale content")
+  Post.create(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
 
 puts "100 posts has been created"
